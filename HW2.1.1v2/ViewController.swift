fde4350680957866a53e725728730f0c21b10f44
darik88 <mr.darik88@gmail.com>
2020-10-27 18:54:25 +0300

    Initial Commit

---
+++ HW2.1.1v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1.1v2
+//
+//  Created by Айдар Рахматуллин on 14.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
